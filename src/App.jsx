895d33fb46c4e792bcf445819c31df599c7f2ba3
rkdranjit <rkdranjit264@gmail.com>
2023-09-18 19:57:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Product from './components/Product'
+
+function App() {
+  
+
+  return (
+    <div className='app'>
+      <Product />
+    </div>
+  )
+}
+
+export default App
